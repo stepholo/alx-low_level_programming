0cfd123deafc6c683a5e8baf8eb7e29276723fa5
stepholo <oloobrian89@gmail.com>
2023-02-16 11:07:38 -0800

    A program without puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,21 +3,13 @@
 /**
  * main - prints to string
  * Description: Prints "and that piece of art is useful.." without puts/printf
- * Return: 1
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long 1 = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
-__asm__ ("syscall"
-: "=a" (ret)
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (1));
-return (1);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
 }
