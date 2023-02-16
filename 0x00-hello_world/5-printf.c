e990b365cae8bb7e54ace1b1f9d4ef86d6679f44
stepholo <oloobrian89@gmail.com>
2023-02-16 05:24:59 -0800

    Use printf to print a program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Descriorion: Prints "with proper grammar, but the outcome is a piece of art,\n"
+ * return: 0
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
