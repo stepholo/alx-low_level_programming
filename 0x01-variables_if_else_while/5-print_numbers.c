b40257aafc679ca7691fd3df0b8f57a71d092428
stepholo <oloobrian89@gmail.com>
2023-02-17 10:37:54 -0800

    Updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers with base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
