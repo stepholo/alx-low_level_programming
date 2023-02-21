72b5f833891449fab8af5991821ecdb9310d21fb
stepholo <oloobrian89@gmail.com>
2023-02-21 12:41:37 -0800

    Updated

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints sum of multiple of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
