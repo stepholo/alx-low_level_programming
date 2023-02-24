db4939eb61865fddbf57238d511d1d619bd996c4
stepholo <oloobrian89@gmail.com>
2023-02-24 01:17:51 -0800

    Updated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,29 +7,15 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+			x = -n;
 	}
-	else
-	{
-		m = n;
-	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 100;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
