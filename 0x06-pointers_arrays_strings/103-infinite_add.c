4bc8cdf28000701cf4311be06e6f0426185590a4
stepholo <oloobrian89@gmail.com>
2023-03-02 10:40:11 -0800

    Updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -21,7 +21,8 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
-		num = (*n1 - '0') + tens;
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
