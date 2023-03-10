a319916de04b02c11a9ee4dac12606e24cd13a12
stepholo <oloobrian89@gmail.com>
2023-03-10 06:10:35 -0800

    Updated

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,10 +12,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; argv[i]; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
 
 	return (0);
 }
