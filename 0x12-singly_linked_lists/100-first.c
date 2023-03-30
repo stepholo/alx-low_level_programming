dcd3b258264e34b27c593f7ab1616b6d28ca1592
stepholo <oloobrian89@gmail.com>
2023-03-30 08:36:46 -0700

    Prints before main is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,7 @@ void __attribute__((constructor)) tortoise_hare(void);
 
 tortoise_hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
