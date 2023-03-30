9fb4fb8e589cb6b163a95cf9ecd2f2b1929daeab
stepholo <oloobrian89@gmail.com>
2023-03-30 08:52:21 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,27 @@
-; My comments: This is a code written in assembly language
-; File: 101 - hello_holberton.asm
+; My comments: It is so fun to write in assembly language
+; File: 101-hello_holberton.asm
 ; Auth: Stephen Oloo
 ; Desc: 64-bit assembly program that prints
-;	Hello, Holberton followed by a new line.
+;       Hello, Holberton followed by a new line.
 
 extern printf
 
 section .text
-global main
+   global main
 
 main:
-	push rbp
+   push rbp
 
-	mov rdi, fmt
-	mov rsi, msg
-	mov rax,0
-	call printf
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
 
-	pop rbp
+   pop rbp
 
-	mov rax,0
-	ret
+   mov rax,0
+   ret
 
 section .data
-	msg: db "Hello, Holberton", 0
-	ftm: db "%s", 10, 0
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
