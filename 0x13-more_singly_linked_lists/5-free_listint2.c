29c39efa1a65353602227350faafca3d681b4bb9
stepholo <oloobrian89@gmail.com>
2023-04-05 13:36:54 -0700

    A function that frees a node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,9 +14,11 @@ void free_listint2(listint_t **head)
 	}
 
 	listint_t *current = *head;
+
 	while (current != NULL)
 	{
 		listint_t *next = current->next;
+
 		free(current);
 		current = next;
 	}
