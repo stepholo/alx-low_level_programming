5c0acdc6fcc497c4d3b4b18dfe30a52b4707c481
stepholo <oloobrian89@gmail.com>
2023-06-15 23:07:14 -0700

    a function that returns the nth node of a dlistint_t linked list.

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
+* @head: Pointer to the head of the list.
+* @index: Index of the node, starting from 0
+*
+* Return: The nth node of the list, or NULL if it doesn't exist
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int count = 0;
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+
+		current = current->next;
+		count++;
+	}
+
+	return (NULL);
+}
