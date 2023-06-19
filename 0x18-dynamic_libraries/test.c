664f28210ab548698c1fbf7026a4287bcc62d75b
stepholo <oloobrian89@gmail.com>
2023-06-19 07:50:02 -0700

    C function to be run in python program

---
+++ 0x18-dynamic_libraries/test.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+* add - Adds two integers
+* @a: First integer
+* @b: Second integer
+* Return: The sum of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* Sub - Subtracts two integers
+* @a: First integer
+* @b: Second integer
+* Return: The the difference of a and b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* Mul- Multiplies two integers
+* @a: First integer
+* @b: Second integer
+* Return: The the number of a times b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divides two integers
+* @a: First integer
+* @b: Second integer
+* Return: a divided by b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - Modulo of two integers
+* @a: First integer
+* @b: Second integer
+* Return: The remainder of dividing a and b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
